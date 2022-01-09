7216d5eccf90234370d3f88db0dd8efa162a6382
gehatz <georgehatzi7@gmail.com>
2022-01-09 17:11:22 -0500

    Configure Solution for N-tier Architecture and create DataAccess, Models, and Utility projects alongside the web project.

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
+
