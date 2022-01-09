1f954db9a846683606518fef5781d2ac680faeb1
gehatz <georgehatzi7@gmail.com>
2022-01-09 16:33:40 -0500

    Implement categories full crud functinalities and global toastr messaging.

---
+++ Bulky.Web/Controllers/CategoryController.cs
@@ -0,0 +1,131 @@
+﻿using Bulky.Web.Data;
+using Bulky.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The name and display order fields must be different.");
+
+                ModelState.AddModelError("Name", "The name and display order fields must be different.");
+                ModelState.AddModelError("DisplayOrder", "The name and display order fields must be different.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(category);
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The name and display order fields must be different.");
+
+                ModelState.AddModelError("Name", "The name and display order fields must be different.");
+                ModelState.AddModelError("DisplayOrder", "The name and display order fields must be different.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(category);
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Bulky.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bulky.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
