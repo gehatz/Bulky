e895c9a71957208ba10593b412e5657f8fc833c4
gehatz <georgehatzi7@gmail.com>
2022-01-09 23:16:17 -0500

    Implement CoverType. Correct issue with Toastr. Minor UI improvements.

---
+++ Bulky.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,133 @@
+﻿using Bulky.DataAccess;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Web.Controllers;
+
+[Area("Admin")]
+public class CategoryController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        IEnumerable<Category> categories = _unitOfWork.Category.GetAll();
+        return View(categories);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category category)
+    {
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The name and display order fields must be different.");
+
+            ModelState.AddModelError("Name", "The name and display order fields must be different.");
+            ModelState.AddModelError("DisplayOrder", "The name and display order fields must be different.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Category.Add(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(category);
+        }
+
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The name and display order fields must be different.");
+
+            ModelState.AddModelError("Name", "The name and display order fields must be different.");
+            ModelState.AddModelError("DisplayOrder", "The name and display order fields must be different.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Category.Update(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(category);
+        }
+
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteCategory(int id)
+    {
+        var category = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        _unitOfWork.Category.Remove(category);
+        _unitOfWork.Save();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
+
---
+++ Bulky.Web/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,117 @@
+﻿using Bulky.DataAccess;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Web.Controllers;
+
+[Area("Admin")]
+public class CoverTypeController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CoverTypeController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        IEnumerable<CoverType> coverType = _unitOfWork.CoverType.GetAll();
+        return View(coverType);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(CoverType coverType)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.CoverType.Add(coverType);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover Type created successfully";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(coverType);
+        }
+
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var coverType = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+        if (coverType == null)
+        {
+            return NotFound();
+        }
+
+        return View(coverType);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(CoverType coverType)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.CoverType.Update(coverType);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover Type updated successfully";
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return View(coverType);
+        }
+
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var coverType = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+        if (coverType == null)
+        {
+            return NotFound();
+        }
+
+        return View(coverType);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteCoverType(int id)
+    {
+        var coverType = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+        if (coverType == null)
+        {
+            return NotFound();
+        }
+
+        _unitOfWork.CoverType.Remove(coverType);
+        _unitOfWork.Save();
+        TempData["success"] = "Cover Type deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
+
