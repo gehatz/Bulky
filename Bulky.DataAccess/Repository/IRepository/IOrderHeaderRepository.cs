53794c52ba523039e51de6e8ad24b4f60c5527cd
gehatz <georgehatzi7@gmail.com>
2022-01-15 18:44:00 -0500

    Implement Strike Payment and Order Confirmation

--- Bulky.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -11,5 +11,6 @@ namespace Bulky.DataAccess.Repository.IRepository
     {
         void Update(OrderHeader orderHeader);
         void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+        void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId);
     }
 }
