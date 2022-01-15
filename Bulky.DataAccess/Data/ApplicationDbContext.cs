f1ea877623b2654cce1e425685a03bbde30f3ce2
gehatz <georgehatzi7@gmail.com>
2022-01-14 22:04:55 -0500

    Extend Identity User DB migration

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext : IdentityDbContext
     {
     }
 
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<CoverType> CoverTypes { get; set; }
     public DbSet<Product> Products { get; set; }
